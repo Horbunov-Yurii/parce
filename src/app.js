87acc0951ac78ce226329580daa381a9a2e335cb
Horbunov Yurii <urijnet@gmail.com>
2025-09-05 15:38:20 +0300

    handlebars

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log("Hello world!");
+import books from "./books.json";
+import bocksTemplate from "./boock-templete.hbs";
+
+
+const listRef = document.querySelector(".list-js");
+
+listRef.innerHTML = bocksTemplate({books})
